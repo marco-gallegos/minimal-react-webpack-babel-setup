0bbb3f88b780835068281782623f2718488a5646
Marco A. Gallegos <ma_galeza@hotmail.com>
2025-04-13 07:10:59 +0000

    chore(project): up and running

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = ({ title }) => <div>{title}</div>;
+const App = ({ title }) => {
 
+    const [count, setCount] = useState(0)
+
+    const add = (e) => {
+        const newCount = count + 1
+        setCount(newCount);
+        console.debug(newCount)
+    }
+
+    return (
+        <div>
+            <div>
+                {title} {count}
+            </div>
+            <button onClick={() => add()} >Add</button>
+        </div>
+    );
+}
 export default App;
